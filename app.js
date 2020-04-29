547998b9efa1040dc8c31719e330e654f03cd31f
federicoig <federicoignacio1@gmail.com>
2020-04-29 14:06:03 -0300

    add cards and buttons

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 let toDoList = document.querySelector("#to-do-list")
+let buttons = document.querySelector(".buttons")
 let paragraph = document.createElement("p")
+let checkButton = document.createElement("i")
+checkButton.setAttribute("onclick", "done()")
+let deleteButton = document.createElement("i")
+checkButton.className = "fas fa-check fa-2x"
+deleteButton.className = "fas fa-trash fa-2x"
 paragraph.className = "to-do"
+let activities = []
 
 function addAnimation(button){
     button.classList.add("clicked-button")
-    setTimeout(function(){removeAnimation(button)}, 550)
+    setTimeout(function(){removeAnimation(button)}, 400)
     button.classList.remove("unclick-button")
 }
+    function removeAnimation(button){
+        button.classList.remove("clicked-button")
+        button.classList.add("unclick-button")
+    }
 
-function removeAnimation(button){
-    button.classList.remove("clicked-button")
-    button.classList.add("unclick-button")
+function addActivities(){
+    let activity = document.querySelector("#activity")
+    if (activity.value === ""){
+        console.log("ERROR")
+    }
+    else {
+        paragraph.innerHTML = activity.value
+        activities.push(paragraph)
+        toDoList.appendChild(paragraph.cloneNode(true))
+        toDoList.appendChild(checkButton.cloneNode(true))
+        toDoList.appendChild(deleteButton.cloneNode(true))
+        activity.value = ""
+    }
 }
 
-function addActivities(button){
-    let activity = document.querySelector("#activity")
-    toDoList.appendChild(paragraph)
-    paragraph.innerHTML = activitie.innerHTML
+function done(){
+    let toDo = document.querySelectorAll(".to-do")
+    toDo.style.backgroundColor = "#00c853"
+    toDo.style.transition = "0.3s ease-in"
+    toDo.style.textDecoration = "line-through"
 }
 
