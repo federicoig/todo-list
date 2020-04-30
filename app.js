a987e54fb12e0a6656d5bb6fd4df0b7b6ba9e0fd
federicoig <federicoignacio1@gmail.com>
2020-04-30 19:53:09 -0300

    final buttons, added the check and delete features, new styles

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-let toDoList = document.querySelector("#to-do-list")
-let buttons = document.querySelector(".buttons")
-let paragraph = document.createElement("p")
-let checkButton = document.createElement("i")
-checkButton.setAttribute("onclick", "done()")
-let deleteButton = document.createElement("i")
-checkButton.className = "fas fa-check fa-2x"
-deleteButton.className = "fas fa-trash fa-2x"
-paragraph.className = "to-do"
-let activities = []
+const button = document.querySelector("form.button")
+const todoList = document.querySelector(".todo-list")
 
 function addAnimation(button){
+    button.classList.remove("button:hover")
     button.classList.add("clicked-button")
-    setTimeout(function(){removeAnimation(button)}, 400)
-    button.classList.remove("unclick-button")
+    setTimeout(function(){removeAnimation(button)}, 450)
 }
-    function removeAnimation(button){
-        button.classList.remove("clicked-button")
-        button.classList.add("unclick-button")
-    }
+
+function removeAnimation(button){
+    button.classList.remove("clicked-button")
+}
 
 function addActivities(){
     let activity = document.querySelector("#activity")
@@ -25,19 +17,60 @@ function addActivities(){
         console.log("ERROR")
     }
     else {
-        paragraph.innerHTML = activity.value
-        activities.push(paragraph)
-        toDoList.appendChild(paragraph.cloneNode(true))
-        toDoList.appendChild(checkButton.cloneNode(true))
-        toDoList.appendChild(deleteButton.cloneNode(true))
+        //Todo DIV
+        const todoDiv = document.createElement("div")
+        todoDiv.classList.add("todo")
+        //Create li
+        const newTodo = document.createElement("li")
+        newTodo.classList.add("newTodo")
+        newTodo.innerText = activity.value
+        todoDiv.appendChild(newTodo)
+        //Check button
+        const completeButton = document.createElement("i")
+        completeButton.innerHTML = "<i class='fas fa-check x2'></i>"
+        completeButton.classList.add("complete-button")
+        todoDiv.appendChild(completeButton)
+        //Delete button
+        const deleteButton = document.createElement("i")
+        deleteButton.innerHTML = "<i class='fas fa-trash x2'></i>"
+        deleteButton.classList.add("delete-button")
+        todoDiv.appendChild(deleteButton)    
+        //append everything
+        todoList.appendChild(todoDiv)
         activity.value = ""
     }
 }
 
-function done(){
-    let toDo = document.querySelectorAll(".to-do")
-    toDo.style.backgroundColor = "#00c853"
-    toDo.style.transition = "0.3s ease-in"
-    toDo.style.textDecoration = "line-through"
+todoList.addEventListener("click", deleteActivity)
+
+function deleteActivity(event){
+    const item = event.target
+    if (item.classList[0] === "delete-button"){
+        const activity = item.parentElement
+        activity.style.opacity = "0"
+        setTimeout(function(){activity.remove();}, 700);
+    }
+}
+
+todoList.addEventListener("click", completeActivity)
+
+function completeActivity(event){
+    const item = event.target
+    if (item.classList[0] === "complete-button"){
+        const activity = item.previousSibling
+        activity.style.transition = "0.3s ease-in-out"
+        activity.style.backgroundColor = "#00c853"
+        activity.style.textDecoration = "line-through"
+    }
+}
+
+let activity = document.querySelector("#activity")
+activity.addEventListener("keydown", addButton)
+
+function addButton(event){
+    if (event.keyCode === 13){
+        addActivities()
+        event.preventDefault() 
+    }   
 }
 
